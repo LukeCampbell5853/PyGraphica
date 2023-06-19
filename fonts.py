44783de2c585dc85dd39d1918b7474e9562b895f
Luke Campbell <64815494+LukeCampbell5853@users.noreply.github.com>
2023-06-19 19:55:12 +1000

    Add files via upload

---
+++ fonts.py
@@ -0,0 +1,22 @@
+Arial = "my_fonts/arial.ttf"
+#Arial font is owned by Monotype imaging
+Baskerville = "my_fonts/baskerville.ttf"
+#Baskerville font was designed by JohnBaskerville in the 1750s
+BrushScript = "my_fonts/brushscript.otf"
+#BrushScript font is owned by Adobe Originals
+Calibri = "my_fonts/calibri.ttf"
+#Calibri is owned by Microsoft
+Courier = "my_fonts/courier.ttf"
+#Courier is owned  by IBM
+Garamond = "my_fonts/garamond.ttf"
+#Garamond is owned by Monotype imaging
+Helvetica  = "my_fonts/helvetica.ttf"
+#Helvetica is owned by Monotype imaging
+Impact = "my_fonts/impact.ttf"
+#Impact is owned by Monotype imaging
+OpenDyslexic = "my_fonts/opendyslexic.otf"
+#OpenDyslexic is owned by Abelardo Gonzales
+TimesNewRoman= "my_fonts/timesnewroman.ttf"
+#TimesNewRoman is owned by Monotype Imagine
+Trebuchet = "my_fonts/timesnewroman.ttf"
+#Trebuchet is owned by Microsoft
